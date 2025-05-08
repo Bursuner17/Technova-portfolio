e5803a4f1dda5d7e715ae2f6193f279041192900
Saitoti Busuna <saitotibursuna1999@gmail.com>
2025-05-08 13:36:00 +0300

    removed backend code

--- portfolio-backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-// backend/routes/contactRoutes.js
-const express = require('express');
-const router = express.Router();
-const { sendMessage } = require('../controllers/contactController');
-
-router.post('/', sendMessage); // POST /api/contact
-
-module.exports = router;
